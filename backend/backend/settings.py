fd82f4ea605d6783484d928fb2e7c3d97b945ab8
xuanphong230104 <nguyenxuanphong230104@gmail.com>
2024-09-13 16:35:47 +0700

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,17 +126,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#configure DRF
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
-}
-
-# configure Djoser
-DJOSER = {
-    "USER_ID_FIELD": "username"
-}
--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+      # 
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser', # remember about comma!
+    # 
+    'apps.accounts' # add new application
 ]
 
 MIDDLEWARE = [
@@ -118,3 +124,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#configure DRF
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+# configure Djoser
+DJOSER = {
+    "USER_ID_FIELD": "username"
+}
